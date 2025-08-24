4bf1d09d0cc789f5abd484aea8f82f28e4a86cbc
lepmoranrp <phillip.moran@rocketpartners.io>
2025-08-24 16:21:25 +0800

    feat(init): Added base code for routing, components. styling, theming and a little bit more of infrastructure revamp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () => import("./pages/home/home").then(m => m.Home),
+  },
+  {
+    path: "contact",
+    loadComponent: () => import("./pages/contact/contact").then(m => m.Contact),
+  },
+  {
+    path: "about",
+    loadComponent: () => import("./pages/about/about").then(m => m.About),
+  },
+  {
+    path: "**",
+    loadComponent: () => import("./pages/page-not-found/page-not-found").then(m => m.PageNotFound),
+  }
+];
